12548cfd0fcce68c0959b71574288cd6199abea1
frances515 <frances.antwi-donkor@ashesi.edu.gh>
2018-03-18 13:37:11 +0000

    get away

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
-import { AngularFireModule } from 'angularfire2';
+import { AddUserPage } from '../pages/add-user/add-user';
+// import { FIREBASE_CREDENTIALS } from './firebase.credentials';
 import { AngularFireAuthModule } from 'angularfire2/auth'
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+//import { AngularFireAuthModule } from 'angularfire2/auth'
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 const firebaseAuth = {
     apiKey: "AIzaSyBqcpOPeRJAbbbACiStw2sjdMJWaSIbjxw",
     authDomain: "timeline-6e270.firebaseapp.com",
-    databaseURL: "https://timeline-6e270.finrebaseio.com",
+    databaseURL: "https://timeline-6e270.firebaseio.com",
     projectId: "timeline-6e270",
     storageBucket: "timeline-6e270.appspot.com",
     messagingSenderId: "268210978652"
@@ -24,7 +28,8 @@ const firebaseAuth = {
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddUserPage
   ],
   imports: [
     BrowserModule,
@@ -32,11 +37,13 @@ const firebaseAuth = {
     AngularFireModule.initializeApp(firebaseAuth),
     AngularFireAuthModule,
     AngularFireDatabaseModule
+ 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddUserPage
   ],
   providers: [
     StatusBar,
